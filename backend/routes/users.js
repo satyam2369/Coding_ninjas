c9bf950af0c6eb7cf481713daf15f812096bffa0
satyam2369 <102215552+satyam2369@users.noreply.github.com>
2024-07-19 20:42:24 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const user = require('../Models/users');
+const user = require('../models/user');
 const { createToken, getDataFromToken } = require('../service/userService');
 var router = express.Router();
 
